0a46d976a41afab71571ef2449a04a11f80fed73
Oleksandr Pivtorak <Sanjok150397@gmail.com>
2025-09-01 02:13:53 +0300

    Enable Whitenoise storage for static files

--- config/settings/prod.py
+++ config/settings/prod.py
@@ -9,6 +9,7 @@ RENDER_EXTERNAL_HOSTNAME = os.environ.get("RENDER_EXTERNAL_HOSTNAME")
 if RENDER_EXTERNAL_HOSTNAME:
    ALLOWED_HOSTS.append(RENDER_EXTERNAL_HOSTNAME)
 
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
